a708f49ce6bd4eb16559ed60bc455e13cf342ddd
Gerry Demaret <gerry@fosdem.org>
2016-11-13 20:46:44 +0100

    Improve bmd-monitor
    Also watch for excessive "no signal" lines in bmd-streamer.log

--- ansible-2017/playbooks/roles/videobox/templates/bmd-monitor/bmd-monitor.sh
+++ ansible-2017/playbooks/roles/videobox/templates/bmd-monitor/bmd-monitor.sh
@@ -2,17 +2,22 @@
 
 SERVICE='bmd-streamer.service'
 LOGFILE='/mnt/ssd/bmd-streamer.log'
+BMDTIMEOUT=15
 
 touch $LOGFILE
 
 # If we catch anything new in the bmdstreamer log
-while inotifywait -e modify $LOGFILE; do
+while inotifywait -e modify $LOGFILE 2>>/dev/null ; do
     # Check if it's a 'no signal' event being logged
-    if tail -n1 $LOGFILE | grep 'stopping encoder'; then
-        service bmdstreamer stop
+
+    NOSIGNAL=$(tail -n $BMDTIMEOUT $LOGFILE | grep "no signal$" | wc -l)
+    STOPPEDENCODER=$(tail -n1 $LOGFILE | grep 'stopping encoder' | wc -l)
+
+    if [ $NOSIGNAL -eq $BMDTIMEOUT ] || [ $STOPPEDENCODER -eq 1 ]; then
+        service bmd-streamer stop
 	sleep 10s
 	#TODO notify someone
 	echo 'Monitoring service attempting to restart bmd-streamer...' > $LOGFILE
-	service bmdstreamer start
+	service bmd-streamer start
     fi
 done
