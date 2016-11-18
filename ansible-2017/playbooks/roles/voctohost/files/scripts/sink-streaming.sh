7894bebee5aec932605fe230489c703b2ad290a3
Gerry Demaret <gerry@fosdem.org>
2016-11-18 19:24:13 +0100

    Add a vocto sink for streaming

---
+++ ansible-2017/playbooks/roles/voctohost/files/scripts/sink-streaming.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+confdir="`dirname "$0"`/../config/"
+. ${confdir}/config.sh
+
+ffmpeg -y -nostdin \
+	-i tcp://localhost:15000 \
+	-threads:0 0 \
+	-aspect 16:9 \
+	-c:v libx264 \
+	-maxrate:v:0 2000k -bufsize:v:0 8192k \
+	-pix_fmt:0 yuv420p -profile:v:0 main -b:v 512k \
+	-preset:v:0 ultrafast \
+	\
+	-ac 1 -c:a libfdk_aac -b:a 96k -ar 44100 \
+	-map 0:v \
+	-map 0:a -filter:a:0 pan=mono:c0=FL \
+	-ac:a:2 2 \
+	\
+	-y -f flv rtmp://${ROOM}.restream.fosdem.org:1935/st/${ROOM}
