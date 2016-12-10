d392f0a0da82dc51090c8f0cec4a49d7c16e9588
Vasil Kolev <vasil@ludost.net>
2016-12-10 19:11:15 +0100

    fix the bandwidth to get a better picture

--- ansible-2017/playbooks/roles/videobox/templates/video-streamer/video-streamer.sh
+++ ansible-2017/playbooks/roles/videobox/templates/video-streamer/video-streamer.sh
@@ -11,4 +11,4 @@ echo 81921024 > /proc/sys/net/core/wmem_default
 echo 81921024 > /proc/sys/net/core/rmem_max
 echo 81921024 > /proc/sys/net/core/rmem_default
 
-/usr/local/bin/bmd-streamer -f /usr/lib/firmware -k 1000 -S hdmi -F 0 2>> ${LOGFILE} | ffmpeg -i - -c copy -f mpegts "${STREAM_DESTINATION}&${STREAM_OPTIONS}"
+/usr/local/bin/bmd-streamer -f /usr/lib/firmware -k 4000 -S hdmi -F 0 2>> ${LOGFILE} | ffmpeg -i - -c copy -f mpegts "${STREAM_DESTINATION}&${STREAM_OPTIONS}"
