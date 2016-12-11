3b392cb5c3a17853aec8d343f9eef98f78d5860f
Gerry Demaret <gerry@fosdem.org>
2016-12-11 14:01:55 +0100

    Also copy the certificates from our private repo

--- ansible-2017/update-private-files.sh
+++ ansible-2017/update-private-files.sh
@@ -10,6 +10,8 @@ echo -n "copying private files from ${1}... "
 cp $1/bmd-firmware/bmd-atemtvstudio.bin playbooks/roles/videobox/files/bmd/bmd-atemtvstudio.bin
 cp $1/bmd-firmware/bmd-h264prorecorder.bin playbooks/roles/videobox/files/bmd/bmd-h264prorecorder.bin
 cp $1/certificates/stream.fosdem.org.key playbooks/roles/streamer-frontend/files/nginx/certificate/stream.fosdem.org.key
+cp $1/certificates/stream.fosdem.org.crt playbooks/roles/streamer-frontend/files/nginx/certificate/stream.fosdem.org.crt
 cp $1/certificates/live.fosdem.org.key playbooks/roles/web-frontend/files/nginx/certificate/live.fosdem.org.key
+cp $1/certificates/live.fosdem.org.crt playbooks/roles/web-frontend/files/nginx/certificate/live.fosdem.org.crt
 
 echo "done."
