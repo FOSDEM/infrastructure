0c39979c3149de56024990a7a29524f141d11075
Gerry Demaret <gerry@fosdem.org>
2016-12-12 21:42:51 +0100

    Bye voctohost, hello voctop

--- ansible-2017/playbooks/roles/voctohost/templates/vocto-scripts/config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-SOURCE_CAM="{{ clone_config.cam_source }}"
-SOURCE_SLIDES="{{ clone_config.slides_source }}"
-SOURCE_URL_PARAMETERS="{{ vocto_container_source_url_parameters }}"
-STREAM_DESTINATION="{{ clone_config.stream_destination }}"
-RECORDING_DIRECTORY="{{ vocto_container_recording_directory }}"
-HOST="vocto-{{ clone_config.room }}"
-ROOM="{{ clone_config.room }}"
