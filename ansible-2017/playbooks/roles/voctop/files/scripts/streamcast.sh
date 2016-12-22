16ba37f1d1010c1c74b13402999a0eaffbd48092
Mark Van den Borre <mark@markvdb.be>
2016-12-22 10:40:31 +0100

    add aac robustness filter

--- ansible-2017/playbooks/roles/voctop/files/scripts/streamcast.sh
+++ ansible-2017/playbooks/roles/voctop/files/scripts/streamcast.sh
@@ -5,5 +5,5 @@ confdir="`dirname "$0"`/../config/"
 . ${confdir}/config.sh
 
 while true; do
-	ffmpeg -y -i "${MULTICAST_SINK}${SOURCE_URL_PARAMETERS}" -c copy -f flv ${STREAM_DESTINATION}
+	ffmpeg -y -flags +global_header -bsf:a aac_adtstoasc -i "${MULTICAST_SINK}${SOURCE_URL_PARAMETERS}" -c copy -f flv ${STREAM_DESTINATION}
 done
