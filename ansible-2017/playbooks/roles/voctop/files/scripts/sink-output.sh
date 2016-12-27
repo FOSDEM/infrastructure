f2138ff8e11593ebcee7c73e3cbfc8e5c24ab7c4
Mark Van den Borre <mark+github@markvdb.be>
2016-12-27 12:46:00 +0100

    set I frame every 45 frames

--- ansible-2017/playbooks/roles/voctop/files/scripts/sink-output.sh
+++ ansible-2017/playbooks/roles/voctop/files/scripts/sink-output.sh
@@ -9,6 +9,7 @@ ffmpeg -y -nostdin \
 	-threads:0 0 \
 	-aspect 16:9 \
 	-c:v libx264 \
+	-g 45 \
 	-maxrate:v:0 2000k -bufsize:v:0 8192k \
 	-pix_fmt:0 yuv420p -profile:v:0 main -b:v 512k \
 	-preset:v:0 veryfast \
