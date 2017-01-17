5fab6441ecdcbe1e58fa77e869f7015a7b6c06b7
Vasil Kolev <vasil@ludost.net>
2017-01-17 17:49:34 +0200

    add control server key/cert

--- ansible-2017/update-private-files.sh
+++ ansible-2017/update-private-files.sh
@@ -13,6 +13,8 @@ cp $1/certificates/stream.fosdem.org.key playbooks/roles/streamer-frontend/files
 cp $1/certificates/stream.fosdem.org.crt playbooks/roles/streamer-frontend/files/nginx/certificate/stream.fosdem.org.crt
 cp $1/certificates/live.fosdem.org.key playbooks/roles/web-frontend/files/nginx/certificate/live.fosdem.org.key
 cp $1/certificates/live.fosdem.org.crt playbooks/roles/web-frontend/files/nginx/certificate/live.fosdem.org.crt
+cp $1/certificates/control.video.fosdem.org.key playbooks/roles/control-server/files/nginx/certificate/control.video.fosdem.org.key
+cp $1/certificates/control.video.fosdem.org.crt playbooks/roles/control-server/files/nginx/certificate/control.video.fosdem.org.crt
 cp $1/sreview/config.pl playbooks/roles/streamer-backend/files/sreview/config.pl
 
 echo "done."
