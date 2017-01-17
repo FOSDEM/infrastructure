7f13151faf09810a3ab1a43d8320e4b517d37391
Wouter Verhelst <w@uter.be>
2017-01-17 12:53:56 +0100

    Copy the sreview config file, too

--- ansible-2017/update-private-files.sh
+++ ansible-2017/update-private-files.sh
@@ -13,5 +13,6 @@ cp $1/certificates/stream.fosdem.org.key playbooks/roles/streamer-frontend/files
 cp $1/certificates/stream.fosdem.org.crt playbooks/roles/streamer-frontend/files/nginx/certificate/stream.fosdem.org.crt
 cp $1/certificates/live.fosdem.org.key playbooks/roles/web-frontend/files/nginx/certificate/live.fosdem.org.key
 cp $1/certificates/live.fosdem.org.crt playbooks/roles/web-frontend/files/nginx/certificate/live.fosdem.org.crt
+cp $1/sreview/config.pl playbooks/roles/streamer-backend/files/sreview/config.pl
 
 echo "done."
