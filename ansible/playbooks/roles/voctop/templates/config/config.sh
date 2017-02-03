d9fc0554a6b17fd839466d26e8c2cccbe3ab4fa4
Gerry Demaret <gerry@fosdem.org>
2017-02-03 15:18:07 +0100

    Fix vocto deployment: hosts are vocto-foo.v.f.o
    Hosts are vocto-foo, not voctop-foo.

--- ansible/playbooks/roles/voctop/templates/config/config.sh
+++ ansible/playbooks/roles/voctop/templates/config/config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 {% for config in vocto_config %}
-{% if inventory_hostname == "voctop-" + config.room + ".video.fosdem.org" %}
+{% if inventory_hostname == "vocto-" + config.room + ".video.fosdem.org" %}
 SOURCE_CAM="{{ config.cam_source }}"
 SOURCE_SLIDES="{{ config.slides_source }}"
 SOURCE_URL_PARAMETERS="{{ vocto_source_url_parameters }}"
