1580e62e138734609548e8e1da3091c847359acb
Vasil Kolev <vasil@ludost.net>
2017-02-05 08:20:54 +0100

    switch to 2mbps

--- ansible/playbooks/roles/videobox/templates/video-streamer/bmd-receiver.sh
+++ ansible/playbooks/roles/videobox/templates/video-streamer/bmd-receiver.sh
@@ -10,4 +10,4 @@ echo 81921024 > /proc/sys/net/core/wmem_default
 echo 81921024 > /proc/sys/net/core/rmem_max
 echo 81921024 > /proc/sys/net/core/rmem_default
 
-/usr/local/bin/bmd-streamer -f /usr/lib/firmware -k 4000 -S hdmi -F 0 2>> ${LOGFILE} | ffmpeg -i - -c copy -f tee -map 0:v -map 0:a '[f=mpegts]'"udp://127.0.0.1:4000?${STREAM_OPTIONS}"'|[f=mpegts]'"udp://127.0.0.1:4001?${STREAM_OPTIONS}"
+/usr/local/bin/bmd-streamer -f /usr/lib/firmware -k 2000 -S hdmi -F 0 2>> ${LOGFILE} | ffmpeg -i - -c copy -f tee -map 0:v -map 0:a '[f=mpegts]'"udp://127.0.0.1:4000?${STREAM_OPTIONS}"'|[f=mpegts]'"udp://127.0.0.1:4001?${STREAM_OPTIONS}"
