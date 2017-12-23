25e3ca0a7b34c2d03b89529b83e47fa938b53f4b
Gerry Demaret <gerry@fosdem.org>
2017-12-23 16:07:13 +0100

    video-box: don't hardcode the SSD path

--- ansible/playbooks/roles/video-box/templates/video-streamer/video-recorder.sh
+++ ansible/playbooks/roles/video-box/templates/video-streamer/video-recorder.sh
@@ -9,4 +9,4 @@ echo 81921024 > /proc/sys/net/core/wmem_default
 echo 81921024 > /proc/sys/net/core/rmem_max
 echo 81921024 > /proc/sys/net/core/rmem_default
 
-ffmpeg -i "udp://127.0.0.1:4000?${STREAM_OPTIONS}&timeout=10000000" -c copy -f mpegts /mnt/ssd/log.`date +%s`.ts
+ffmpeg -i "udp://127.0.0.1:4000?${STREAM_OPTIONS}&timeout=10000000" -c copy -f mpegts {{ ssd_mount }}/video-recording/log.`date +%s`.ts
