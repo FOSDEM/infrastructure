b5113f34cd5be9788593809111102d41adc94ddf
Wouter Verhelst <w@uter.be>
2018-01-27 19:13:10 +0100

    Also show the free working storage stuff

--- ansible/playbooks/roles/encoder-backend/files/load.sh
+++ ansible/playbooks/roles/encoder-backend/files/load.sh
@@ -7,9 +7,11 @@ do
 		exit
 	fi
 	tmpfree=$(df --output=avail /tmp | tail -n1)
+	storagefree=$(df --output=avail /srv/sreview/storage | tail -n1)
 	me=$(hostname -f)
 
 	echo begin
 	echo ${me}:tmpfree:${tmpfree}k
+	echo global:storage:${storage}k
 	echo end
 done
