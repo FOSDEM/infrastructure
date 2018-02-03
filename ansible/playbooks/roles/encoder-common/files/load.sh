705df04d53af916a204a80bae19e777d171eeb38
Wouter Verhelst <w@uter.be>
2018-02-03 19:40:21 +0100

    Also query the available diskspace on the second storage node

--- ansible/playbooks/roles/encoder-common/files/load.sh
+++ ansible/playbooks/roles/encoder-common/files/load.sh
@@ -8,10 +8,12 @@ do
 	fi
 	tmpfree=$(df --output=avail /tmp | tail -n1)
 	storagefree=$(df --output=avail /srv/sreview/storage | tail -n1)
+	st2=$(df --output=avail /srv/sreview/storage/1/2018-02-04 | tail -n1)
 	me=$(hostname -f)
 
 	echo begin
 	echo ${me}:tmpfree:${tmpfree}k
 	echo global:storagefree:${storagefree}k
+	echo global:storagefree2:${st2}k
 	echo end
 done
