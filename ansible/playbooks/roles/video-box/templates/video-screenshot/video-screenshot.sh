5417e9e7e8472ee90f905e27507b579db45e3dd1
Vasil Kolev <vasil@ludost.net>
2018-12-01 11:04:36 +0100

    videbox/screenshot service: atomic overwrite

--- ansible/playbooks/roles/video-box/templates/video-screenshot/video-screenshot.sh
+++ ansible/playbooks/roles/video-box/templates/video-screenshot/video-screenshot.sh
@@ -3,8 +3,8 @@
 SCREENSHOT_PATH="{{ video_screenshot_directory }}/{{ video_screenshot_filename }}"
 
 while true; do
-	# Make a screenshot
-	ffmpeg -i tcp://localhost:8898/?timeout=10000 -vf scale=160:-1 -qscale:v 20 -vframes 1 -y $SCREENSHOT_PATH 2>> /dev/null
+	# Make a screenshot and move atomically
+	ffmpeg -i tcp://localhost:8898/?timeout=10000 -vf scale=160:-1 -qscale:v 20 -vframes 1 -y ${SCREENSHOT_PATH}.$$.jpg 2>> /dev/null && mv ${SCREENSHOT_PATH}.$$ ${SCREENSHOT_PATH}.jpg
 
 	# If we failed, make sure there is no file at all
 	if [ $? -ne 0 ]; then
@@ -12,6 +12,7 @@ while true; do
 			rm $SCREENSHOT_PATH
 		fi
 	fi
+	rm -f ${SCREENSHOT_PATH}.$$.jpg
 
 	sleep 2s
 done
