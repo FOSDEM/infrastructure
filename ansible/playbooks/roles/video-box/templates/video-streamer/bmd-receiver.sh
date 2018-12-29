6ae410120c968631f6af53f8c282336d7e32d9ee
Gerry Demaret <gerry@fosdem.org>
2018-12-29 14:43:05 +0100

    video-box: reduce ffmpeg verbosity, less logging

--- ansible/playbooks/roles/video-box/templates/video-streamer/bmd-receiver.sh
+++ ansible/playbooks/roles/video-box/templates/video-streamer/bmd-receiver.sh
@@ -9,4 +9,4 @@ echo 81921024 > /proc/sys/net/core/wmem_default
 echo 81921024 > /proc/sys/net/core/rmem_max
 echo 81921024 > /proc/sys/net/core/rmem_default
 
-/usr/local/bin/bmd-streamer -f /usr/lib/firmware -k 2000 -S hdmi -F 0 2>> ${LOGFILE} | ffmpeg -i - -c copy -f mpegts - | /usr/local/bin/sproxy
+/usr/local/bin/bmd-streamer -f /usr/lib/firmware -k 2000 -S hdmi -F 0 2>> ${LOGFILE} | ffmpeg -v error -i - -c copy -f mpegts - | /usr/local/bin/sproxy
--- ansible/playbooks/roles/video-box/templates/video-streamer/video-recorder.sh
+++ ansible/playbooks/roles/video-box/templates/video-streamer/video-recorder.sh
@@ -7,4 +7,4 @@ echo 81921024 > /proc/sys/net/core/wmem_default
 echo 81921024 > /proc/sys/net/core/rmem_max
 echo 81921024 > /proc/sys/net/core/rmem_default
 
-ffmpeg -i "tcp://127.0.0.1:8899?timeout=10000000" -c copy -f mpegts {{ ssd_mount }}/video-recording/log.`date +%s`.ts
+ffmpeg -v error -i "tcp://127.0.0.1:8899?timeout=10000000" -c copy -f mpegts {{ ssd_mount }}/video-recording/log.`date +%s`.ts
