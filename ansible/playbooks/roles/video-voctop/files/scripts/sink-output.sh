e55ad5ad3e5e068103569dac1699d54dfdbe7ba1
Vasil Kolev <vasil@ludost.net>
2019-11-24 14:39:24 +0100

    voctop: switch to hardware/gpu decoding (pointy-hat-to: mark)

--- ansible/playbooks/roles/video-voctop/files/scripts/sink-output.sh
+++ ansible/playbooks/roles/video-voctop/files/scripts/sink-output.sh
@@ -4,20 +4,20 @@ confdir="`dirname "$0"`/../config/"
 . ${confdir}/defaults.sh
 . ${confdir}/config.sh
 
-ffmpeg -v error -y -nostdin \
+ffmpeg -y -nostdin -init_hw_device vaapi=intel:/dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device intel -filter_hw_device intel  \
 	-probesize 10M \
 	-analyzeduration 10M \
 	-i tcp://localhost:11000 \
 	-threads:0 0 \
 	-aspect 16:9 \
-	-c:v libx264 \
+	-filter_complex "[0:a]channelsplit=channel_layout=stereo[left][right]; [0:v] format=nv12,hwupload [vout]" \
+	-map '[vout]' \
+	-c:v:0 h264_vaapi \
 	-g 45 \
-	-filter_complex "[0:a]channelsplit=channel_layout=stereo[left][right]" \
 	-maxrate:v:0 2000k -bufsize:v:0 8192k \
-	-pix_fmt:0 yuv420p -profile:v:0 main -b:v 512k \
-	-preset:v:0 veryfast \
+	-b:v:0 1000k \
+	-qmin:v:0 1 \
 	\
-	-map 0:v \
 	-map '[left]:1' \
 	-ac 1 -strict -2 -c:a aac -b:a 128k -ar 48000 \
 	-map '[right]:2' \
--- ansible/playbooks/roles/video-voctop/files/scripts/source-slides.sh
+++ ansible/playbooks/roles/video-voctop/files/scripts/source-slides.sh
@@ -4,11 +4,11 @@ confdir="`dirname "$0"`/../config/"
 . ${confdir}/defaults.sh
 . ${confdir}/config.sh
 
-ffmpeg -v error -y -nostdin \
+ffmpeg -y -nostdin -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device /dev/dri/renderD128 \
 	-i "${SOURCE_SLIDES}" \
 	-ac 2 \
 	-filter_complex "
-		[0:v] scale=$WIDTH:$HEIGHT,fps=$FRAMERATE,setdar=16/9,setsar=1 [v] ;
+		[0:v]  scale_vaapi=w=$WIDTH:h=$HEIGHT  [v2]; [v2] hwdownload,format=nv12,format=yuv420p [v1]; [v1] scale=$WIDTH:$HEIGHT,fps=$FRAMERATE,setdar=16/9,setsar=1 [v] ;
 		[0:a] aresample=$AUDIORATE [a]
 	" \
 	-map "[v]" -map "[a]" \
