0388ee08e340c427ae87f3e399605000a02bb692
Vasil Kolev <vasil@ludost.net>
2019-11-24 19:35:17 +0100

    videobox: remove obsolete sysctl setting

--- ansible/playbooks/roles/video-box/templates/video-streamer/bmd-receiver.sh
+++ ansible/playbooks/roles/video-box/templates/video-streamer/bmd-receiver.sh
@@ -2,11 +2,4 @@
 
 LOGFILE="/mnt/ssd/bmd-streamer.log"
 
-# The default socket size is too small
-echo 81921024 > /proc/sys/net/core/wmem_max
-echo 81921024 > /proc/sys/net/core/wmem_default
-
-echo 81921024 > /proc/sys/net/core/rmem_max
-echo 81921024 > /proc/sys/net/core/rmem_default
-
 /usr/local/bin/bmd-streamer -f /usr/lib/firmware -k 2000 -S hdmi -F 0 2>> ${LOGFILE} | ffmpeg -v error -i - -c copy -f mpegts - | /usr/local/bin/sproxy
--- ansible/playbooks/roles/video-box/templates/video-streamer/video-recorder.sh
+++ ansible/playbooks/roles/video-box/templates/video-streamer/video-recorder.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-# The default socket size is too small
-echo 81921024 > /proc/sys/net/core/wmem_max
-echo 81921024 > /proc/sys/net/core/wmem_default
-
-echo 81921024 > /proc/sys/net/core/rmem_max
-echo 81921024 > /proc/sys/net/core/rmem_default
-
 ffmpeg -v error -i "tcp://127.0.0.1:8899?timeout=10000000" -c copy -f mpegts {{ ssd_mount }}/video-recording/log.`date +%s`.ts
