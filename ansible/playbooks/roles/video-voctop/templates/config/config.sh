b1970e97ae847be0fd6c386a661d3dde488b0a2d
Vasil Kolev <vasil@ludost.net>
2019-11-24 16:30:03 +0100

    voctop: move the simpler config

--- ansible/playbooks/roles/video-voctop/templates/config/config.sh
+++ ansible/playbooks/roles/video-voctop/templates/config/config.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-{% for config in vocto_config %}
-{% if inventory_hostname ==  config.room + "-voctop.video.fosdem.org" %}
-SOURCE_CAM="tcp://{{ config.room }}-cam.local:8898/?timeout=2000000"
-SOURCE_SLIDES="tcp://{{ config.room }}-slides.local:8898/?timeout=2000000"
+{% for room in video_rooms %}
+{% if inventory_hostname ==  room + "-voctop.video.fosdem.org" %}
+SOURCE_CAM="tcp://{{ room }}-cam.local:8898/?timeout=2000000"
+SOURCE_SLIDES="tcp://{{ room }}-slides.local:8898/?timeout=2000000"
 SOURCE_URL_PARAMETERS="{{ vocto_source_url_parameters }}"
-STREAM_DESTINATION="{{ config.stream_destination }}"
-STREAM_BACKUP_DESTINATION="{{ config.stream_backup_destination }}"
 RECORDING_DIRECTORY="{{ vocto_recording_directory }}"
-HOST="{{ config.room }}-voctop"
-ROOM="{{ config.room }}"
+HOST="{{ room }}-voctop"
+ROOM="{{ room }}"
 {% endif %}
 {% endfor %}
