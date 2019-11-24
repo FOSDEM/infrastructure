f6f5090d69a7a874fa131799bdba7bce070577ff
Vasil Kolev <vasil@ludost.net>
2019-11-24 14:26:37 +0100

    backend: fix filenames, paths

--- ansible/playbooks/roles/video-streamer-backend/files/fetcher/fetcher.sh
+++ ansible/playbooks/roles/video-streamer-backend/files/fetcher/fetcher.sh
@@ -5,10 +5,12 @@ if [ -z "$2" ]; then
 	exit 2
 fi
 
+cd /var/www/hls || exit 3
+
 src="$1"
 room="$2"
 
 ffmpeg -v error -nostdin -y -i "$src" \
 	-c copy -map 0:v:0 -map 0:a:0 -map 0:a:1 \
-	-f tee '[f=hls:hls_flags=delete_segments+temp_file:hls_start_number_source=datetime:hls_time=5:hls_delete_threshold=5]'${room}'.m3u8|[f=segment:segment_time=1800:segment_format=mpegts:strftime=1]/var/www/dump/'${room}/${room}'-%Y%m%d%H%M%S.ts' 
+	-f tee '[f=hls:hls_flags=delete_segments+temp_file:hls_start_number_source=datetime:hls_time=5:hls_delete_threshold=5:hls_segment_filename='${room}-'%d.ts]'${room}'.m3u8|[f=segment:segment_time=1800:segment_format=mpegts:strftime=1]/var/www/dump/'${room}/${room}'-%Y%m%d%H%M%S.ts' 
 
