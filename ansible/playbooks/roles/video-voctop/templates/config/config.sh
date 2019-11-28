15e6ecca4554e6769421d03109af6136c17b244c
Vasil Kolev <vasil@ludost.net>
2019-11-29 00:42:22 +0200

    voctop/box: add HTTP support
    As we'll be using some other boxes in a few rooms, the easiest way was to
    have support for their interface (http://.../0.ts) to sproxy. Currently
    it uses port 8080, but if the other hardware can't change port, I'll update
    it to 80.

--- ansible/playbooks/roles/video-voctop/templates/config/config.sh
+++ ansible/playbooks/roles/video-voctop/templates/config/config.sh
@@ -2,8 +2,8 @@
 
 {% for room in video_rooms %}
 {% if inventory_hostname ==  room + "-voctop.video.fosdem.org" %}
-SOURCE_CAM="tcp://{{ room }}-cam.local:8898/?timeout=2000000"
-SOURCE_SLIDES="tcp://{{ room }}-slides.local:8898/?timeout=2000000"
+SOURCE_CAM="http://{{ room }}-cam.video.fosdem.org:8080/0.ts"
+SOURCE_SLIDES="http://{{ room }}-slides.video.fosdem.org:8080/0.ts"
 SOURCE_URL_PARAMETERS="{{ vocto_source_url_parameters }}"
 RECORDING_DIRECTORY="{{ vocto_recording_directory }}"
 HOST="{{ room }}-voctop"
