f71c774cb9efac4e18b54db5dc8ad4acb9204ad3
Wouter Verhelst <w@uter.be>
2020-01-22 12:55:33 +0200

    Drop storagefree2 and storagefree3
    These no longer exist, so...

--- ansible/playbooks/roles/encoder-common/files/load.sh
+++ ansible/playbooks/roles/encoder-common/files/load.sh
@@ -8,14 +8,10 @@ do
 	fi
 	tmpfree=$(df --output=avail /tmp | tail -n1)
 	storagefree=$(df --output=avail /srv/sreview/storage | tail -n1)
-	st2=$(df --output=avail /srv/sreview/storage/1/2019-02-03 | tail -n1)
-	st3=$(df --output=avail /srv/sreview/extra | tail -n1)
 	me=$(hostname -f)
 
 	echo begin
 	echo ${me}:tmpfree:${tmpfree}k
 	echo global:storagefree:${storagefree}k
-	echo global:storagefree2:${st2}k
-        echo global:storagefree3:${st3}k
 	echo end
 done
