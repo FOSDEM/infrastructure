17ed5d6d1b7f9f2befbb0a680cc5a88c298db0ca
Johan Van de Wauw <johan@gisky.be>
2020-01-29 16:27:07 +0100

    Integrate room full api in video control

--- ansible/playbooks/roles/video-control-server/files/vocto.php
+++ ansible/playbooks/roles/video-control-server/files/vocto.php
@@ -34,7 +34,16 @@ if (empty($_GET['w']) && empty($argv[1])) {
 <head><title>room <?php echo $room; ?></title>
 </head>
 <body>
-
+<div>
+<form>
+<label for="room_status">Room Status:</label>
+   <select id="room_status">
+    <option value="" disabled selected></option>
+	<option value="0">empty</option>
+	<option value="1">full</option>
+   </select>
+</form>
+</div>
 <table>
 <tr>
 <td>	<form method=GET target="tgt" action="/vocto.php" style="float: left;"><input hidden name="room" value="<?php echo $room;?>"><input hidden name="w" value="ssp-p"><input type="submit" style="height:50px" value="side-by-side presenter"></form></td>
@@ -61,6 +70,23 @@ if (empty($_GET['w']) && empty($argv[1])) {
 <script>var img1 = document.getElementsByTagName('img')[0];var src1=img1.src;img1.addEventListener('load', function() {setTimeout(function() {img1.src=src1+'?'+Date.now()}, 1000)})</script>
 <script>var img2 = document.getElementsByTagName('img')[1];var src2=img2.src;img2.addEventListener('load', function() {setTimeout(function() {img2.src=src2+'?'+Date.now()}, 1000)})</script>
 <script>var img3 = document.getElementsByTagName('img')[2];var src3=img3.src;img3.addEventListener('load', function() {setTimeout(function() {img3.src=src3+'?'+Date.now()}, 1000)})</script>
+<script>
+(function() {
+    var room_status = document.getElementById('room_status');
+	room_status.onchange = function(){
+		room_status.value;
+		const http = new XMLHttpRequest();
+		const status_url = "room_status.php?state="+room_status.value;
+		http.open("GET", status_url);
+		http.send();
+
+		http.onreadystatechange=(e)=>{
+			console.log(http.responseText);
+		}
+	}
+})();	
+
+</script>
 <iframe name="tgt" id="target" width="0" height="0"></iframe>
 </body>
 <?php
