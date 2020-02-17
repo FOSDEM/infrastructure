8dc86888a50965582d902d61c557abc1ba3931b6
Wouter Verhelst <w@uter.be>
2020-02-17 14:55:36 +0200

    Add comment to this script so updates don't break review

--- ansible/playbooks/roles/video-voctop/files/scripts/sink-output.sh
+++ ansible/playbooks/roles/video-voctop/files/scripts/sink-output.sh
@@ -4,6 +4,15 @@ confdir="`dirname "$0"`/../config/"
 . ${confdir}/defaults.sh
 . ${confdir}/config.sh
 
+#
+# The video encoding needs to be kept in sync with the review system,
+# otherwise things like
+# https://twitter.com/jlecour/status/1229381883680694273 happen.
+#
+# Either update playbooks/roles/encoder-common/templates/config.j2
+# (extra_profiles variable, FOSDEM key) or talk to Wouter if you can't
+# figure it out. Thanks!
+#
 ffmpeg -y -nostdin -init_hw_device vaapi=intel:/dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device intel -filter_hw_device intel  \
 	-probesize 10M \
 	-analyzeduration 10M \
