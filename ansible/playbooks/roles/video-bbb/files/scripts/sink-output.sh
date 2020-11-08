7a551c99c764ebdd50ec77f874c1497d300e0e5f
Mark Van den Borre <mark@markvdb.be>
2020-11-08 11:55:45 +0100

    strip out hw accelleration for non-intel hw

--- ansible/playbooks/roles/video-bbb/files/scripts/sink-output.sh
+++ ansible/playbooks/roles/video-bbb/files/scripts/sink-output.sh
@@ -13,22 +13,19 @@ confdir="`dirname "$0"`/../config/"
 # (extra_profiles variable, FOSDEM key) or talk to Wouter if you can't
 # figure it out. Thanks!
 #
-ffmpeg -y -nostdin -init_hw_device vaapi=intel:/dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device intel -filter_hw_device intel  \
+ffmpeg -v error -y -nostdin \
 	-probesize 10M \
 	-analyzeduration 10M \
 	-i tcp://localhost:11000 \
 	-threads:0 0 \
 	-aspect 16:9 \
-	-filter_complex "[0:a]channelsplit=channel_layout=stereo[left][right]; [0:v] format=nv12,hwupload [vout]" \
-	-map '[vout]' \
-	-c:v:0 h264_vaapi \
+	-c:v libx264 \
 	-g 45 \
 	-maxrate:v:0 2000k -bufsize:v:0 8192k \
-	-b:v:0 1000k \
-	-qmin:v:0 1 \
+	-pix_fmt:0 yuv420p -profile:v:0 main -b:v 512k \
+	-preset:v:0 veryfast \
 	\
-	-map '[left]:1' \
-	-ac 1 -strict -2 -c:a aac -b:a 128k -ar 48000 \
-	-map '[right]:2' \
-	-ac 1 -strict -2 -c:a aac -b:a 128k -ar 48000 \
+	-ac 2 -strict -2 -c:a aac -b:a 128k -ar 48000 \
+	-map 0:v \
+	-map 0:a \
 	-y -f mpegts - | /usr/local/bin/sproxy
