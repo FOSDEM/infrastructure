82effb15160b2a3b8c99086a4e680efefce2b492
Mark Van den Borre <mark@markvdb.be>
2020-11-08 12:06:13 +0100

    video source: add comment with source video stream

--- ansible/playbooks/roles/video-bbb/templates/config/config.sh
+++ ansible/playbooks/roles/video-bbb/templates/config/config.sh
@@ -2,6 +2,9 @@
 
 {% for room in video_rooms %}
 {% if inventory_hostname ==  room + "-voctop.video.fosdem.org" %}
+# If you want to test use these:
+# SOURCE_CAM="http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8"
+# SOURCE_SLIDES="http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8"
 SOURCE_CAM="http://{{ room }}-cam.video.fosdem.org/0.ts"
 SOURCE_SLIDES="http://{{ room }}-slides.video.fosdem.org/0.ts"
 SOURCE_URL_PARAMETERS="{{ vocto_source_url_parameters }}"
