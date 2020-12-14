dae30e29bb1eedca22674ff2c07c7a839cb348f6
Mark Van den Borre <mark+github@markvdb.be>
2020-12-14 21:12:40 +0100

    add recording source script
    Not very different; just integrates well with new systemd timer & service

---
+++ ansible/playbooks/roles/video-bbb/files/scripts/source-recording.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+confdir="`dirname "$0"`/../config/"
+. ${confdir}/defaults.sh
+. ${confdir}/config.sh
+
+ffmpeg -y -nostdin \
+	-i "${SOURCE_RECORDING_DIR}/${1}.mp4" \
+	-ac 2 \
+	-filter_complex "
+		[0:v] scale=$WIDTH:$HEIGHT,fps=$FRAMERATE,setdar=16/9,setsar=1 [v] ;
+		[0:a] aresample=$AUDIORATE [a]
+	" \
+	-map "[v]" -map "[a]" \
+	-pix_fmt yuv420p \
+	-c:v rawvideo \
+	-c:a pcm_s16le \
+	-f matroska \
+	tcp://localhost:10000
