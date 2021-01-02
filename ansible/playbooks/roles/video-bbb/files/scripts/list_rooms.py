ea121c90d7492f856ba73e94b9d96a2f4002ccb6
Mark Van den Borre <mark@markvdb.be>
2021-01-02 11:56:09 +0100

    add vocto hosts per virtualised FOSDEM room

---
+++ ansible/playbooks/roles/video-bbb/files/scripts/list_rooms.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python3
+ 
+from slugify import slugify
+import json
+import os
+import sys
+import wget
+import xml.etree.ElementTree as ET 
+
+
+
+penta_url = 'https://fosdem.org/2021/schedule/xml'        
+print("Checking if we have the schedule...")
+if os.path.isfile(os.path.basename(penta_url)):
+  penta = os.path.basename(penta_url)
+else:
+  print("Getting the schedule...")
+  penta = wget.download(penta_url)
+
+print("Parsing the schedule file...")
+pentaparse = ET.parse(penta).getroot()
+rooms=sorted(list(set([slugify(r.get('name')) for r in pentaparse.findall('.//room') if r.get('name')])))
+
+output=''
+
+print('Creating ansible host entries')
+for r in rooms:
+    output += r+'-vocto.video.fosdem.org\n'
+
+f = open('/tmp/ansible_hosts_voctos', 'w')
+f.write(output)
+f.close()
