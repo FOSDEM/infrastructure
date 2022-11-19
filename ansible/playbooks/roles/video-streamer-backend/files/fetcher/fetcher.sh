10534bcddd29b4e8eefb8ac67cad349fa8d5a866
Vasil Kolev <vasil@ludost.net>
2022-11-19 11:49:38 +0100

    stream-backend: query control for where the voctop is

--- ansible/playbooks/roles/video-streamer-backend/files/fetcher/fetcher.sh
+++ ansible/playbooks/roles/video-streamer-backend/files/fetcher/fetcher.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-if [ -z "$2" ]; then
-	echo Usage: $0 source room
+if [ -z "$1" ]; then
+	echo Usage: $0 room
 	exit 2
 fi
 
 cd /var/www/hls || exit 3
 
-src="$1"
-room="$2"
+room="$1"
+
+src=$(curl http://control.video.fosdem.org/query-room.php?room=$room)
+
+if [ -z "$src" ]; then
+	# we don't exist, sleep for a while
+	sleep 60
+fi
+
 
 ffmpeg -v error -nostdin -y -i "$src" \
 	-c copy -map 0:v:0 -map 0:a:0 \
