2b435814174948c3fd249219dd483c4065c6175e
Vasil Kolev <vasil@ludost.net>
2022-11-19 12:04:24 +0100

    voctop: non-static box/slides config

---
+++ ansible/playbooks/roles/video-voctop/files/config/config.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+data=$(curl -s http://control.video.fosdem.org/query-vocto.php?voctop=$(hostname))
+
+if [ -z "$data" ] || [ "$data" == "not found" ]; then
+	# we don't exist. wait.
+	sleep 60
+	exit
+fi
+
+room=$(echo $data | cut -d ' ' -f 1)
+cam=$(echo $data | cut -d ' ' -f 2)
+slides=$(echo $data | cut -d ' ' -f 3)
+
+SOURCE_CAM=http://${cam}/0.ts
+SOURCE_SLIDES=http://${slides}/0.ts
