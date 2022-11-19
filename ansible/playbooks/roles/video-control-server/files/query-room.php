45eb45c79e72ace6c67c729accf1253b61f118d1
Vasil Kolev <vasil@ludost.net>
2022-11-19 11:49:38 +0100

    stream-backend: query control for where the voctop is
    also add API for the query

---
+++ ansible/playbooks/roles/video-control-server/files/query-room.php
@@ -0,0 +1,17 @@
+<?php
+
+include "inc.php";
+
+if(empty($_REQUEST['room'])) {	
+	die("who are you (room param)");
+}
+$room = $_REQUEST['room'];
+
+$r = pg_query("SELECT voctop FROM fosdem WHERE roomname='"._e($room)."'");
+
+if (!$r) {
+	die("not found");
+}
+
+$row = pg_fetch_row($r);
+echo 'tcp://'.$row[0].':8899/?timeout=3000000';
