caf99bf10b0ed6ff1908882b2fd4f92b679b25d4
Vasil Kolev <vasil@ludost.net>
2022-11-19 11:30:08 +0100

    floating rooms: initial
    * make some services already fetch which room them're assigned
      based on hostname
    * touch audio-fetcher to be python3
    * remove prometheus support from audio-fetcher
    * audio-fetcher refreshes room name
    * imgmaker refreshes room name
    * extra supporting services on control
    * initial web api on control

--- ansible/playbooks/roles/video-control-server/files/imgmaker.sh
+++ ansible/playbooks/roles/video-control-server/files/imgmaker.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-if [ -z "$2" ]; then
-	echo "Usage: $0 roompath host"
+if [ -z "$1" ]; then
+	echo "Usage: $0 room"
 	exit 2
 fi
+cd /var/www/html
 
 ROOM=$1
-HOST=$2
+HOST=$(psql -A -t -q fosdem -c "select voctop from fosdem where roomname='$ROOM'")
+
+if [ -z "$HOST" ]; then
+	# we have no voctop for this room, do not spin
+	sleep 60
+	exit 0
+fi
 
 t=`tempfile`
 rm -f ${t} ${t}.jpg
 
 ffmpeg=`which ffmpeg`
 if ! [ $? -eq 0 ]; then
-	ffmpeg=`which avconv`
-	if ! [ $? -eq 0 ]; then
-		echo No ffmpeg/avconv found.
-		exit 3
-	fi
+	echo No ffmpeg/avconv found.
+	exit 3
 fi
 
 
@@ -28,7 +32,15 @@ function ff_fetch {
 
 mkdir -p ${ROOM}
 
+# exit after this many times to reload voctop
+times=30
+i=0
+
 while /bin/true; do
+	let i=$i+1
+	if [ $i -gt $times ]; then
+		exit 0
+	fi
 	ff_fetch $HOST 11000 ${ROOM}/room.jpg
 	ff_fetch $HOST 13000 ${ROOM}/cam.jpg
 	ff_fetch $HOST 13001 ${ROOM}/grab.jpg
