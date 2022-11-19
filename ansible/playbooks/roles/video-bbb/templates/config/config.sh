d43fa26a94c46eda0fe3461473afb8f0f0d58c6a
Vasil Kolev <vasil@ludost.net>
2022-11-19 12:51:37 +0100

    video-bbb: remove a lot of unused varbles and the need for video_rooms

--- ansible/playbooks/roles/video-bbb/templates/config/config.sh
+++ ansible/playbooks/roles/video-bbb/templates/config/config.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
 
-{% for room in video_rooms %}
-{% if inventory_hostname ==  room + "-vocto.video.fosdem.org" %}
-# If you want to test use these:
-# SOURCE_CAM="http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8"
-# SOURCE_SLIDES="http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8"
-SOURCE_CAM="http://{{ room }}-cam.video.fosdem.org/0.ts"
-SOURCE_SLIDES="http://{{ room }}-slides.video.fosdem.org/0.ts"
-SOURCE_URL_PARAMETERS="{{ vocto_source_url_parameters }}"
 RECORDING_DIRECTORY="{{ vocto_recording_directory }}"
-HOST="{{ room }}-vocto"
-ROOM="{{ room }}"
-{% endif %}
-{% endfor %}
