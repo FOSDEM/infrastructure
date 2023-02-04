44313e7e75b2b1c9019f9ab70919c9d161acb5cc
Vasil Kolev <vasil@ludost.net>
2023-02-04 12:55:51 +0100

    streambackend: missed second audio channel for physical rooms

--- ansible/playbooks/roles/video-streamer-backend/files/fetcher/fetcher.sh
+++ ansible/playbooks/roles/video-streamer-backend/files/fetcher/fetcher.sh
@@ -22,8 +22,13 @@ if [ -z "$src" ]; then
 	sleep 60
 fi
 
+if echo  $room |grep -q ^d; then
+	extraaudio=""
+else
+	extraaudio="-map 0:a:1"
+fi
 
 ffmpeg -v error -nostdin -y -i "$src" \
-	-c copy -map 0:v:0 -map 0:a:0 \
+	-c copy -map 0:v:0 -map 0:a:0 ${extraaudio} \
 	-f tee '[f=hls:hls_flags=delete_segments+temp_file:hls_start_number_source=datetime:hls_time=2:hls_delete_threshold=10:hls_segment_filename='${room}-'%d.ts]'${room}'.m3u8|[f=segment:segment_time=1800:segment_format=mpegts:strftime=1]/var/www/dump/'${room}/${room}'-%Y%m%d%H%M%S.ts' 
 
