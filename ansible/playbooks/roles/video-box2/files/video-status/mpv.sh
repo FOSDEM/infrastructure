e882a2b8aea31e7a7c7f327579f3f8f6ed2dd849
Vasil Kolev <vasil@ludost.net>
2023-05-28 15:03:28 +0200

    video-box2: we want the status singnal be as close to the real time as possible

--- ansible/playbooks/roles/video-box2/files/video-status/mpv.sh
+++ ansible/playbooks/roles/video-box2/files/video-status/mpv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export DISPLAY=:0.0
-mpv --stop-screensaver=no --ao=null --geometry=50%-20+20 --vd-lavc-fast --vd-lavc-skiploopfilter=all --vd-lavc-skipframe=nonref --vd-lavc-framedrop=nonref --no-ytdl --quiet --demuxer-lavf-analyzeduration=30 --lavfi-complex='[aid1] asplit [t1] [ao] ; [t1] showvolume=w=700:h=100 [t2] ; [vid1]  [t2]  overlay  [vo]' tcp://localhost:8898
+mpv --stop-screensaver=no --ao=null --geometry=50%-20+20 --vd-lavc-fast --vd-lavc-skiploopfilter=all --vd-lavc-skipframe=nonref --vd-lavc-framedrop=nonref --no-ytdl --quiet --demuxer-lavf-analyzeduration=30 --lavfi-complex='[aid1] asplit [t1] [ao] ; [t1] showvolume=w=700:h=100 [t2] ; [vid1]  [t2]  overlay  [vo]' tcp://localhost:8899
