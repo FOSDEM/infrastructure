9572c346f54fb11451bf1a82bf9ba716ba8f9a35
Gerry Demaret <gerry@fosdem.org>
2023-10-21 13:40:42 +0200

    video-box2: rename to video-box, remove old role

--- ansible/playbooks/roles/video-box/templates/video-streamer/bmd-receiver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-LOGFILE="/mnt/ssd/bmd-streamer.log"
-
-/usr/local/bin/bmd-streamer -f /usr/lib/firmware -k 2500 -K 3500 -S hdmi -F 0 2>> ${LOGFILE} | ffmpeg -v error -i - -c copy -f mpegts - | /usr/bin/sproxy
--- ansible/playbooks/roles/video-box/templates/video-streamer/video-recorder.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ffmpeg -v error -i "tcp://127.0.0.1:8899?timeout=10000000" -c copy -f mpegts {{ ssd_mount }}/video-recording/log.`date +%s`.ts
