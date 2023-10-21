f0e7d4d901648f6e26e6433d830edabf2f3334fb
Vasil Kolev <vasil@ludost.net>
2023-10-21 20:11:10 +0200

    video-box: signalstatus to use the loop mode of ms213x-status

--- ansible/playbooks/roles/video-box/files/signal-status/statuskeeper.sh
+++ ansible/playbooks/roles/video-box/files/signal-status/statuskeeper.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-while :; do
-	sleep 0.2
-	ms213x-status status --json > /tmp/status.tmp
-	if [ $? -eq 0 ]; then
-		mv /tmp/status.tmp /tmp/ms213x-status
-	fi
-done
+ms213x-status status --json -loop 1000 --filename /tmp/ms213x-status
