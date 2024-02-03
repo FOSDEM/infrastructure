750057a5903e40de13e31c70ba81d9501f12a24c
Vasil Kolev <vasil@ludost.net>
2024-02-03 11:31:06 +0100

    videobox: we're runnig the old firmware

--- ansible/playbooks/roles/video-box/files/signal-status/statuskeeper.sh
+++ ansible/playbooks/roles/video-box/files/signal-status/statuskeeper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ms213x-status status --json --loop 1000 --filename /tmp/ms213x-status --region=bertold
+ms213x-status status --json --loop 1000 --filename /tmp/ms213x-status --region=flaky
