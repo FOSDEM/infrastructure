67cc35891d93d8b1366bc9ba2527de8f001e87f3
Vasil Kolev <vasil@ludost.net>
2025-03-29 14:39:54 +0200

    video-box-mixer: fix source-slides to use the config

--- ansible/playbooks/roles/video-box-mixer/files/scripts/source-slides.sh
+++ ansible/playbooks/roles/video-box-mixer/files/scripts/source-slides.sh
@@ -19,17 +19,17 @@ sleep 5
 adev=$(arecord -l  |grep -E 'USB3|Hagibis|HC-336' |cut -d: -f1 |cut -d' ' -f 2)
 vdev=$(v4l2-ctl --list-devices |grep -EA 1 'USB3|Hagibis|HC-336' |tail -n1)
 
-height=$(cat /tmp/ms213x-status | jq -r '1920/( (.width/.height)|if . > 2 then . / 2 else . end)|floor')
+height=$(cat /tmp/ms213x-status | jq -r ${WIDTH}'/( (.width/.height)|if . > 2 then . / 2 else . end)|floor')
 
 /usr/bin/wait_next_second
 
 taskset -c 3 ffmpeg -y -nostdin -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device /dev/dri/renderD128 \
 	-analyzeduration 3M \
-	-f v4l2 -video_size 1920x${height} -framerate 30 -i $vdev \
+	-f v4l2 -video_size ${WIDTH}x${height} -framerate 30 -i $vdev \
 	-itsoffset 0.064 -f alsa -sample_rate 48000 -channels 2 -i hw:$adev \
 	-ac 2 \
 	-filter_complex "
-		[0:v] scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black,fps=$FRAMERATE,setdar=16/9,setsar=1 [v] ;
+		[0:v] scale=${WIDTH}:${HEIGHT}:force_original_aspect_ratio=decrease,pad=${WIDTH}:${HEIGHT}:-1:-1:color=black,fps=$FRAMERATE,setdar=16/9,setsar=1 [v] ;
 		[1:a] aresample=$AUDIORATE [a]
 	" \
 	-map "[v]" -map "[a]" \
