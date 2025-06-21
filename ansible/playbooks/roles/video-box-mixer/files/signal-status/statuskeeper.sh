2b035bd684fa02986ceffb1727785544aa26c0cd
Vasil Kolev <vasil@ludost.net>
2025-06-21 22:03:41 +0200

    video-box-mixer: read the proper status for the slides hagibis
    Also, use the Bertold region

--- ansible/playbooks/roles/video-box-mixer/files/signal-status/statuskeeper.sh
+++ ansible/playbooks/roles/video-box-mixer/files/signal-status/statuskeeper.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ms213x-status status --json --loop 1000 --filename /tmp/ms213x-status --region=flaky
+usbpos=4-2
+upath=/sys/bus/usb/devices/$usbpos
+rdev=/dev/$(basename $(find $upath/*/*/hidraw -mindepth 1 -maxdepth 1 | sort |head -n1)) 
+
+ms213x-status status --json --loop 1000 --filename /tmp/ms213x-status --raw-path=$rdev --region=bertold
