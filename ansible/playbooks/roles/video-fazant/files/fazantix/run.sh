d0493d79342ed20f3af32e554aa71583ae369cb4
Vasil Kolev <vasil@ludost.net>
2026-03-21 22:34:21 +0100

    video-fazant: latest updates
    This seems to run pretty well, for some reason.

--- ansible/playbooks/roles/video-fazant/files/fazantix/run.sh
+++ ansible/playbooks/roles/video-fazant/files/fazantix/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /opt/fazantix
-cage -d -- bash -c 'nice -n -19 systemd-run --scope -p AllowedCPUs=1 fazantix /opt/fazantix/fosdem.yaml'
+#cage -d -- bash -c 'GOEXPERIMENT=preemptibleloops systemd-run --scope -p AllowedCPUs=1 fazantix /opt/fazantix/fosdem.yaml'
+cage -d -- bash -c 'GOEXPERIMENT=preemptibleloops GOMAXPROCS=32  systemd-run --scope -p AllowedCPUs=1 fazantix /opt/fazantix/fosdem.yaml'
